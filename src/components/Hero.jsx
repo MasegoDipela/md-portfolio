fb88094d1bcf505efb025dfc95f79f7ed090d7a0
Masego Dipela <masegolxve@gmail.com>
2024-06-17 16:25:58 +0200

    Cleaned up Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,6 +35,32 @@ const Hero = () => {
             Download CV
           </Button>
         </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <img
+                  src="src\assets\hero\Hashbit.png"
+                  className="sm:w-full w-full lg:scale-[1] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                  width={1014}
+                  height={490}
+                  alt="Hashbit"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:w-[138%] lg:-top-[104%]">
+            <img
+              src="src\assets\hero\Hashbit-background-2.png"
+              className="w-full "
+              width={1440}
+              height={1800}
+              alt="Hashbit Backround"
+            />
+          </div>
+        </div>
       </div>
     </Section>
   );
